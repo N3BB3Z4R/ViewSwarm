ce582fb3c8edcc4df7dd800b61a86c5b89f50d11
Eduardo <eduardo.redondo@wallbox.com>
2021-04-12 10:34:26 +0200

    add background active user

---
+++ getStreamers.js
@@ -0,0 +1,92 @@
+import { streamers } from './streamers.js';
+
+export const getStreamers = (filter) => {
+  // clean embed to html
+  document.getElementById("twitch-embed").innerHTML = "";
+
+  let allChannels = [];
+
+  streamers.forEach(element => {
+    allChannels.push(
+      fetch(`https://api.twitch.tv/kraken/search/streams?query=${element.channel}`, {
+        headers: {
+          accept: 'application/vnd.twitchtv.v5+json',
+        'client-id': 'b31o4btkqth5bzbvr9ub2ovr79umhh',
+        }
+      })
+      .then(response => response.json())
+      .then(({ streams }) => streams))
+  })
+
+  return Promise.all(allChannels).then(streams => {
+    const lives = streams.filter(item => item.length)
+    let warzoneLives = []
+
+    console.log('filter', filter)
+    if (filter === 'affiliate') {
+      warzoneLives = lives.flat().filter(item => (
+        // item.game.includes('Warzone') &&
+        item.channel.broadcaster_type === filter &&
+        hasChannelExist(item.channel.name)
+      ));
+      console.log("affialite", warzoneLives)
+    } else if (filter === 'noAffiliate') {
+      warzoneLives = lives.flat().filter(item => (
+        // item.game.includes('Warzone') &&
+        item.channel.broadcaster_type === '' &&
+        hasChannelExist(item.channel.name)
+      ));
+      console.log("noAffiliate", warzoneLives)
+    } else {
+      warzoneLives = lives.flat().filter(item => hasChannelExist(item.channel.name));
+      console.log("all", warzoneLives)
+    }
+
+    const randomWarzoneLives = shuffleArray(warzoneLives)
+
+    randomWarzoneLives.map(({ channel }, index) => {
+      new Twitch.Embed(`twitch-embed`, {
+        width: 640,
+        height: 480,
+        muted: "yes",
+        channel: `${channel.name}`,
+        // only needed if your site is also embedded on embed.example.com and othersite.example.com
+        parent: ["embed.example.com", "othersite.example.com"],
+      });
+    })
+
+    return warzoneLives
+  })
+}
+
+// Devuelve array de Streamers con orden aleatorio
+function shuffleArray(array) { return array.sort(() => Math.random() - 0.5); }
+
+function hasChannelExist (channel) {
+  return streamers.some(item => item.channel === channel)
+}
+
+export function setUserList (usersActive) {
+  let ul = document.createElement('ul');
+  ul.setAttribute('id','users');
+  document.getElementById('list').appendChild(ul)
+
+  streamers.forEach(channel => {
+    const nameChannel = channel.channel
+    let li = document.createElement('li')
+    li = addBackagroundActive(usersActive, nameChannel, li)
+    ul.appendChild(li)
+    li.innerHTML = nameChannel
+  })
+}
+
+function addBackagroundActive(usersActive, nameChannel, li) {
+  usersActive.forEach(user => {
+    if(user.channel.name === nameChannel) {
+      console.log(1, nameChannel)
+      li.style.backgroundColor = 'green'
+    }
+  })
+
+  return li
+}
