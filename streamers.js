3d6e3017a33e9bd7ca94200aadcc11ea4d786447
Nebe <n3bb3z4r@gmail.com>
2021-04-13 14:05:25 +0200

    update streamers

--- streamers.js
+++ streamers.js
@@ -1867,5 +1867,16 @@ export const streamers = [{
       "broadcast_type": "affiliate",
       "plataforma": "ps4",
       "id-activision": ""
+    },
+    {
+      "name": "Pam_TV",
+      "channel": "pam_tv",
+      "group": "2",
+      "raid": "0",
+      "admin": "0",
+      "showstream": "1",
+      "broadcast_type": "affiliate",
+      "plataforma": "ps4",
+      "id-activision": ""
     }
   ]
