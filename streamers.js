a9ff1a540de19dc44852d6c491456ce7ba1d18d5
Nebe <n3bb3z4r@gmail.com>
2021-04-21 19:26:45 +0200

    updating streamers

--- streamers.js
+++ streamers.js
@@ -2219,5 +2219,16 @@ export const streamers = [{
       "broadcast_type": "affiliate",
       "plataforma": "ps4",
       "id-activision": ""
+    },
+    {
+      "name": "TheEagleShot",
+      "channel": "theeagleshot",
+      "group": "2",
+      "raid": "0",
+      "admin": "0",
+      "showstream": true,
+      "broadcast_type": "affiliate",
+      "plataforma": "ps4",
+      "id-activision": ""
     }
   ]
